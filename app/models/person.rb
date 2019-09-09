b2520552a151c0390db339bcf8fece914f054c43
steve-alex <steve.alex@hotmail.co.uk>
2019-09-09 19:02:17 +0100

    Created book_flights method

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,19 +2,16 @@ class Person < ActiveRecord::Base
     has_many :bookings
     has_many :people, through: :bookings
 
-    def create_booking(origin:, destination:, arrival_time:, departure_time:, booking_price:)
-        #Search results hash keys will have to match create_booking parameters key
-        if Flight.exists?(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
+    def book_flight(origin:, destination:, arrival_time:, departure_time:, price:)
+        if Flight.exists?(origin: origin, destination: destination, departure_time: departure_time, arrival_time: arrival_time)
             flight = Flight.find_by(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
         else
-            Flight.create(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
+            flight = Flight.create(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
         end
-    
-        Booking.create(:flight => flight, :person => self, :booking_price => booking_price)
-        nil
+        Booking.create(:flight => flight, :person => self, :price => price)
     end
 
     def update_booking(new_departure_time)
-    end
+    end 
 
 end
