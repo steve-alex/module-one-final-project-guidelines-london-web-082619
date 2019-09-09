1d3e5f5f33f9215422722a47cf3b5fa99149d80c
Angus Morrison <contact@angus-morrison.com>
2019-09-09 15:03:37 +0100

    Added tty to gemfile

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,16 @@
-require_relative '../config/environment'
+#require_relative '../config/environment'
+require 'tty-prompt'
 
+$prompt = TTY::Prompt.new
 
+def welcome
+    puts "Welcome to Skyscourer ✈️"
+    puts "The fight booking service similar to, but legally distinct from, Skyscanner"
+end
 
-puts "HELLO WORLD"
+def sign_in
+    $prompt.select("To start, sign in or create an account:", %w(Sign\ in "Create\ account))
+end
+
+welcome
+sign_in
