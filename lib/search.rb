c0cbb7ffb3b0eafe927fa5a30b4057c84259aa81
Angus Morrison <contact@angus-morrison.com>
2019-09-09 13:59:07 +0100

    Added Search class to repo. Awaiting final get_airport_from_city function

---
+++ lib/search.rb
@@ -0,0 +1,153 @@
+#Unirest is  a set of lightweight HTTP libraries that allow GET, POST, PUT, PATCH and DELETE requests
+require 'unirest'
+require 'pry'
+require 'JSON'
+require 'active_support/core_ext/hash'
+
+
+class Search
+  attr_reader :origin, :destination, :outbound_date, :inbound_date
+
+  @@max_results = 5
+
+  ###### Instance methods ######
+
+  def initialize(origin, destination, outbound_date, inbound_date="")
+    @origin = origin
+    @destination = destination
+    @outbound_date = outbound_date
+    @search_results = nil
+    Unirest.timeout(30)
+  end
+
+  #Run the search and return an array of unique flight hashes
+  def run_search
+    session = create_session
+    #binding.pry
+    return "Search timed out. Please try again." if !session
+    session_key = get_key(session)
+    get_search_results(session_key)
+    return "Search timed out. Please try again." if !@search_results
+    flights = cheapest_unique_flights(create_flights)
+    #Limit the number of results returned
+    flights.length > @@max_results ? flights.slice(0...@@max_results) : flights
+  end
+
+
+  private
+
+  #POST method: create a Skyscanner session with search parameters
+  def create_session()
+    begin
+      response = Unirest.post(
+        "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+        headers:{
+          "X-RapidAPI-Host" => "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+          "X-RapidAPI-Key" => "c036ae2334msh6e52f9287ee7e7ap1fbff8jsnb8c6fa5b89b5",
+          "Content-Type" => "application/x-www-form-urlencoded"
+        },
+        parameters:{
+          "inboundDate" => self.inbound_date,
+          "cabinClass" => "economy",
+          "children" => 0,
+          "infants" => 0,
+          "country" => "UK",
+          "currency" => "GBP",
+          "locale" => "en-US",
+          "originPlace" => self.origin,
+          "destinationPlace" => self.destination,
+          "outboundDate" => self.outbound_date,
+          "adults" => 1
+        }
+      )
+    rescue RuntimeError
+      return nil
+    end
+    #Call create_session until the response is valid
+    #NEED TO CREATE TIMEOUT!!
+    valid_response?(response) ? response : create_session()
+  end
+
+  #Check that the POST method HTTPResponse header contains a valid :location
+  def valid_response?(response)
+    response.headers[:location]
+  end
+
+  #Extract the session key (string) from the raw POST response
+  def get_key(session)
+    session_url = session.headers[:location]
+    session_url.split("/").last
+  end
+
+  #Poll the session results and save them to the search_results instance variable
+  def get_search_results(session_key)
+    begin
+      raw_results = Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/#{session_key}?sortType=price&sortOrder=asc&stops=0&pageIndex=0&pageSize=10",
+      headers:{
+        "X-RapidAPI-Host" => "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        "X-RapidAPI-Key" => "c036ae2334msh6e52f9287ee7e7ap1fbff8jsnb8c6fa5b89b5"
+      })
+    rescue RuntimeError
+      return nil
+    end
+
+    #Generate search_results hash from XML response
+    @search_results = Hash.from_xml(raw_results.body)
+  end
+
+  #Extract itineraries from search results hash
+  def get_itineraries
+    itin_array = @search_results["PollSessionResponseDto"]["Itineraries"]["ItineraryApiDto"]
+
+    itin_array.each_with_object([]) do | itin, array |
+      flight = {}
+      flight["flight_id"] = itin["OutboundLegId"]
+
+      if itin["PricingOptions"]["PricingOptionApiDto"].is_a?(Array)
+        flight["price"] = itin["PricingOptions"]["PricingOptionApiDto"][0]["Price"]
+      else
+        flight["price"] = itin["PricingOptions"]["PricingOptionApiDto"]["Price"]
+      end
+
+      array << flight
+    end
+  end
+
+
+  #Return an array of legs that match an itinerary (departure time data)
+  def get_legs
+    @search_results["PollSessionResponseDto"]["Legs"]["ItineraryLegApiDto"]
+  end 
+
+  #Combine pricing data with departure time data
+  def create_flights
+    all_flights = get_itineraries.each do | itin |
+      matching_leg = get_legs.find { | leg | leg["Id"] == itin["flight_id"] }
+      itin["departure_time"] = matching_leg["Departure"]
+      itin["arrival_time"] = matching_leg["Arrival"]
+    end
+  end
+
+  #Takes in a list of flights and returns the cheapest flight for each arrival time
+  def cheapest_unique_flights(flights)
+    flights.uniq { | flight | flight["arrival_time"] }
+  end
+
+
+  ###### Class methods ######
+
+  #Return the first skyscanner airport code from a city name
+  def self.get_airport_from_city(city)
+
+  end
+
+
+
+
+
+end
+
+search1 = Search.new("LHR-sky", "SFO-sky", "2020-01-10")
+search = Search.get_airport_from_city("London")
+binding.pry
+0
