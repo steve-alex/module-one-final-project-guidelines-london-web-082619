7a5eaebe852bdf3728238649eee9c93c3d6fe81d
Angus Morrison <contact@angus-morrison.com>
2019-09-11 17:49:20 +0100

    Date validation no longer allows slashes

--- bin/run.rb
+++ bin/run.rb
@@ -246,7 +246,7 @@ class Session
         #Takes a string ("departing" or "returning") and gets departure or return date from user
         date = @prompt.ask("What date are you #{departing_or_returning}? DD-MM-YYYY") do |q|
             q.required true
-            q.validate /(^(((0[1-9]|1[0-9]|2[0-8])[\/\-](0[1-9]|1[012]))|((29|30|31)[\/\-](0[13578]|1[02]))|((29|30)[\/\-](0[4,6,9]|11)))[\/\-](19|[2-9][0-9])\d\d$)|(^29[\/\-]02[\/\-](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/
+            q.validate /(^(((0[1-9]|1[0-9]|2[0-8])[\-](0[1-9]|1[012]))|((29|30|31)[\-](0[13578]|1[02]))|((29|30)[\-](0[4,6,9]|11)))[\-](19|[2-9][0-9])\d\d$)|(^29[\-]02[\-](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/
             q.messages[:valid?] = "Dates must use the format DD-MM-YYYY"
         end
         valid_date?(date) ? date : get_date(departing_or_returning)
