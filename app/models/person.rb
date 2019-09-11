cb5c13dc41cd8a85df39859a12ce21056ad9533d
Angus Morrison <contact@angus-morrison.com>
2019-09-11 19:04:12 +0100

    Moved all require statements to environment.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,4 @@
 class Person < ActiveRecord::Base
     has_many :bookings
     has_many :flights, through: :bookings
-
-    def update_flight(origin:, destination:, arrival_time:, departure_time:, price:)
-        if Flight.exists?(origin: origin, destination: destination, departure_time: departure_time, arrival_time: arrival_time)
-            flight = Flight.find_by(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
-        else
-            flight = Flight.create(origin: origin, destination: destination, arrival_time: arrival_time, departure_time: departure_time)
-        end
-        Booking.update
-        
-    end
-
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
 require 'bundler'
 Bundler.require
 
+require 'unirest'
+require 'pry'
+require 'active_support/core_ext/hash'
+require 'tty-prompt'
+require 'tty-table'
+require 'tty-spinner'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'app'
 require_all 'app/models'
